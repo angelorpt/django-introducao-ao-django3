800203009887d35cb328d49bbe3e0f11e4b6abbc
Angelo Rafael <angelorpt@gmail.com>
2021-02-18 16:23:45 -0400

    add | Arquivos estáticos CSS JS HTML

--- alurareceita/settings.py
+++ alurareceita/settings.py
@@ -13,7 +13,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+import os
 BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -55,7 +57,7 @@ ROOT_URLCONF = 'alurareceita.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'receitas/templates')],        
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -67,6 +69,8 @@ TEMPLATES = [
         },
     },
 ]
+print (BASE_DIR)
+print (TEMPLATES)
 
 WSGI_APPLICATION = 'alurareceita.wsgi.application'
 
@@ -118,4 +122,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'alurareceita/static')
+]
+print (STATIC_ROOT)
+print (STATICFILES_DIRS)
